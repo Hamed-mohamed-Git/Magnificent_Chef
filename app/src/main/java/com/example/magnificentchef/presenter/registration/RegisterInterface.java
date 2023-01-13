d89900bb7b55897ee5ed27e096f7550edf082a8c
AyaMohamedAhmed <ayamohamed3420@gmail.com>
2023-01-13 21:56:48 +0200

    add:signUp with Google

---
+++ app/src/main/java/com/example/magnificentchef/presenter/registration/RegisterInterface.java
@@ -0,0 +1,8 @@
+package com.example.magnificentchef.presenter.registration;
+
+import java.util.List;
+
+public interface RegisterInterface {
+    public void onClick();
+
+}
--- app/src/main/java/com/example/magnificentchef/view/register/RegisterFragment.java
+++ app/src/main/java/com/example/magnificentchef/view/register/RegisterFragment.java
@@ -1,16 +1,26 @@
 package com.example.magnificentchef.view.register;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 import com.example.magnificentchef.R;
+import com.example.magnificentchef.presenter.registration.RegisterInterface;
+import com.example.magnificentchef.presenter.registration.RegisterPresenter;
 
 
-public class RegisterFragment extends Fragment {
-
-
+public class RegisterFragment extends Fragment implements RegisterInterface {
+    Button btn_google;
+    View view;
+    private RegisterPresenter registerPresenter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,5 +33,28 @@ public class RegisterFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_register, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btn_google= view.findViewById(R.id.btn_google);
+        registerPresenter=new RegisterPresenter(getContext(),requireActivity());
+        btn_google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerPresenter.Join();
+            }
+        });
+    }
+
+    @Override
+    public void onClick() {
+//ToDo navigate action put here (modify it)
+        //TODO what well send
+
+        Navigation
+                .findNavController(view)
+                .navigate(R.id.action_splashScreenFragment_to_registerFragment);
     }
 }
